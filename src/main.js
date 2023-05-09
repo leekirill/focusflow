7925e9d62fe05656a9a57e6de60310cf2208ea14
ваше имя <cyril.lee1997@gmail.com>
2023-05-09 22:03:24 +0200

    .

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import PrimeVue from "primevue/config";
 import ToastService from "primevue/toastservice";
 import router from "./router";
 import App from "./App.vue";
-// import "firebase/auth";
-// import "firebase/firestore";
 
 import "./styles.scss";
 import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
